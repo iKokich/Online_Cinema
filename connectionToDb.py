8910945d04ce4fd4f95054436d7a6210fbad0df6
iKokich <slahikelit@gmail.com>
2024-10-11 20:55:33 +0300

    Подключение базы данных и создание: Созданы классы Actor.py,Director.py, Genre.py, Main.py, Movie.py, Order.py, Person.py, User.py

--- connectionToDb.py
+++ connectionToDb.py
@@ -1,40 +1,61 @@
 import psycopg2
 import yaml
 
-def connect_to_db(config_file='db_info.yaml'):
-    """Подключение к базе данных с использованием конфигурации из YAML-файла.
+class DatabaseConnection:
+    def __init__(self, config_file = 'db_info.yaml'):
+        self.config_file = config_file
+        self.conn = None
 
-    Args:
-        config_file (str, optional): Путь к YAML-файлу с конфигурацией. По умолчанию 'config.yaml'.
+    def connection(self):
+        try:
+            with open(self.config_file, 'r') as f:
+                config = yaml.safe_load(f)
+            self.conn = psycopg2.connect(
+                dbname = config['database']['dbname'],
+                user = config['database']['user'],
+                password = config['database']['password'],
+                host = config['database']['host'],
+                port = config['database']['port']
+            )
+            print("Database connected successfully!")
+        except(Exception, psycopg2.Error) as error:
+            print("Error while connecting to PostgreSQL", error)
 
-    Returns:
-        psycopg2.connection.connection: Объект подключения к базе данных.
-    """
+    def close(self):
+        if self.conn:
+            self.conn.close()
+            print("Соединение закрыто")
 
-    try:
-        with open(config_file, 'r') as f:
-            config = yaml.safe_load(f)
+    def execute(self, query, params=None):
+        with self.conn.cursos() as cur:
+            cur.execute(query, params)
+            self.conn.commit()
 
-        conn = psycopg2.connect(
-            dbname=config['database']['dbname'],
-            user=config['database']['user'],
-            password=config['database']['password'],
-            host=config['database']['host'],
-            port=config['database']['port']
-        )
-        print("Database connected successfully!")
-        return conn
-    except (Exception, psycopg2.Error) as error:
-        print("Error while connecting to PostgreSQL", error)
+    def insert(self, table_name, data):
+        columns = ', '.join(data.keys())
+        placeholders = ', '.join(['%s'] * len(data))
+        values = tuple(data.values())
 
-if __name__ == '__main__':
-    conn = connect_to_db()
+        query = f"INSET INTO {table_name} ({columns}) VALUES ({placeholders})"
 
-    if conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM Movies;")
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
-        conn.close()
-        print("Соединение с базой данных закрыто.")
+        self.execute(query, values)
+
+    def update(self, table_name, data, where):
+        set_clause = ', '.join([f"{key} = %s" for key in data])
+        values = tuple(data.values())
+
+        query = f"UPDATE {table_name} SET {set_clause} WHERE {where}"
+        self.execute(query, values)
+
+    def delete(self, table_name, where):
+        query = f"DELETE FROM {table_name} WHERE {where}"
+        self.execute(query)
+
+    def fetchall(self, query):
+        with self.conn.cursor() as cur:
+            cur.execute(query)
+            return cur.fetchall()
+
+    def get_all_data(self, table_name):
+        query = f"SELECT * FROM {table_name}"
+        return self.fetchall(query)
